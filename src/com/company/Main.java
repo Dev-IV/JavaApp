2cd63acc417fad3d617bf889f13aeec3288c5f95
hsdgks <87986036+hsdgks@users.noreply.github.com>
2021-11-02 14:43:10 +0700

    Биография

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Меня зовут игорь, мне 28 лет, я студент, люблю программирование, обожаю плавание!");
+    }
+}
